dcfefe344dfaa7acb6b775bbf519c10216802ac1
Devin Vinicio Diaz <diazdevin7@outlook.com>
2024-04-20 22:03:57 -0400

    Update AnalogSensorReader.ino

--- AnalogSensorReader.ino
+++ AnalogSensorReader.ino
@@ -1,5 +1,9 @@
 /*
-This Arduino script is designed to continuously read analog values from three specific pins (presumably sensors) and print these values to the serial monitor, separated by spaces for clarity. It initializes serial communication at 9600 baud rate in the setup function. In the main loop, it reads values from analog pins 0, 1, and 2 sequentially. After printing each value, it inserts a space for separation. The loop incorporates a delay of 1500 milliseconds (1.5 seconds) between each cycle to make the serial output readable and manageable. This simple utility is useful for monitoring and debugging sensor outputs in real-time, especially in testing environments or initial stages of sensor-related project development.
+This Arduino script is designed to continuously read analog values from three specific pins (presumably sensors) and print these values to the serial monitor, 
+separated by spaces for clarity. It initializes serial communication at 9600 baud rate in the setup function. In the main loop, it reads values from analog
+pins 0, 1, and 2 sequentially. After printing each value, it inserts a space for separation. The loop incorporates a delay of 1500 milliseconds (1.5 seconds) between 
+each cycle to make the serial output readable and manageable. This simple utility is useful for monitoring and debugging sensor outputs in real-time, especially in
+testing environments or initial stages of sensor-related project development.
 */
 
 void setup() {
